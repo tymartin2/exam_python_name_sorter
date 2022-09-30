daf184fbacee361b2d6312fd317231d340250b7b
Marty Miguel <martymiguel@gmail.com>
2022-09-30 18:05:02 +0800

    done with file validation

--- name_sorter.py
+++ name_sorter.py
@@ -1,29 +1,42 @@
+import sys 
 #reader method to read the csv file to be sorted
 import csv
 
-#acquiring the file to read and sort.
-file = open('random_name.csv', 'r')
+args = 2
+cli_args = len(sys.argv)
 
-#convert the csv file to a csv object, with ',' to separate the columns. 
-csv_object = csv.reader(file, delimiter=',')
+if cli_args < args : 
+	print('Argument Missing: 2nd parameter is required')
+else: 
+	#get second argument from cli
+	random_csv = sys.argv[1].lower()
 
-#convert the object to a list allowing the data to be sorted. 
-nested_list = list(csv_object)
+	if random_csv.endswith('.csv') :
+		
+		#acquiring the file to read and sort.
+		file = open(random_csv, 'r')
 
-#alphabetically sorted data.  
-sorted_list = sorted(nested_list, key=lambda x: x[1])
+		#convert the csv file to a csv object, with ',' to separate the columns. 
+		csv_object = csv.reader(file, delimiter=',')
 
-#close the sorter file. 
-file.close()
+		#convert the object to a list allowing the data to be sorted. 
+		nested_list = list(csv_object)
 
-#open a new file where the sorted data will be written and stored.
-new_csv = open('sorted_names.csv', 'w+', newline='') 
+		#alphabetically sorted data.  
+		sorted_list = sorted(nested_list, key=lambda x: x[1])
 
-#to create a writer object to convert the input/data into strings.
-csv_writer = csv.writer(new_csv, delimiter=',')
+		#close the sorter file. 
+		file.close()
 
-#to identify the data that will be placed in each column.
-csv_writer.writerow(['Given Name', 'Last Name'])
+		#open a new file where the sorted data will be written and stored.
+		new_csv = open('sorted_names.csv', 'w+', newline='') 
+		#to create a writer object to convert the input/data into strings.
+		csv_writer = csv.writer(new_csv, delimiter=',')
 
-#display the sorted data into the new csv file. 
-csv_writer.writerows(sorted_list)
+		#to identify the data that will be placed in each column.
+		csv_writer.writerow(['Given Name', 'Last Name'])
+
+		#display the sorted data into the new csv file. 
+		csv_writer.writerows(sorted_list)
+	else :
+		print('Incorrect File: csv file required')
